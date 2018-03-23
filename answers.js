271c7481c9f898784303b082f29dd7f007611c7e
nworbnitsuj <justin.v.brown@gmail.com>
2018-03-23 09:52:38 -0500

    Commit 1 - Verbal questions

--- answers.js
+++
@@ -1,6 +0,0 @@
-let i = 1;
-
-while (i <=20) {
-  console.log(i);
-  i++;
-}
---
+++ week-one/thursday/solutions.js
@@ -0,0 +1,5 @@
+// What is the difference between a parameter and an argument?
+// "Parameter" refers to the variables that take "arguments", which are the values assigned to the variables.
+//
+// Within a function, what is the difference between return and console.log?
+// The return statement ends a function's execution and specifies a value to be returned to the function caller, while console.log prints the value to the console.
