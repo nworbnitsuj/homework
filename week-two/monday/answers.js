8a549bd904bc5eb3b8f94049a686a8d3c44e2ccb
nworbnitsuj <justin.v.brown@gmail.com>
2018-03-27 08:57:56 -0500

    1st commit - selecting datatypes

---
+++ week-two/monday/answers.js
@@ -0,0 +1,10 @@
+// Week 2: Tuesday Homework
+
+// Datatypes
+
+1. Boolean, lightSwitch = true; toggleOnOff () => {if (lightswitch===true) {lightSwitch=false}else{lightSwitch=true}}
+2. String, email: "justin.v.brown@gmail.com"
+3. Object, let hull = {armor: "steel", power: "nuclear"} let warpSpeed = {speed: "light-speed", chargeTimeInSeconds: 30}
+4. Array, let studentNames = ["Justin", "VP", "Spencer"]
+5. Object, let students = {{name: "Justin", location: "Austin"}{name: "VP", location: "Austin"}{name: "Spencer", location: "Austin"}}
+6. Object, let students = {{name: "Justin", location: "Austin" favMovie: "Casino"}{name: "Spencer", location: "Austin", favMovie: "Good Fellas"}}
