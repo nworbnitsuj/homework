67cfa0ce7cc90fbab1ab28836a84af13956b9817
nworbnitsuj <justin.v.brown@gmail.com>
2018-03-27 09:06:16 -0500

    3rd commit - performed operations on the crazy object

--- week-two/monday/answers.js
+++ week-two/monday/answers.js
@@ -20,3 +20,10 @@
     datatype: "objects"
 }
 4. console.log(thisGuy["hobby"]);
+
+// Crazy Object
+1. console.log(crazyObject.taco[1].salsa[5]);
+2. console.log(crazyObject.larry.quotes[0]);
+3. console.log(crazyObject.larry.characters[2].favourtieHobby);
+4. console.log(crazyObject.larry.nicknames[1]);
+5. console.log(crazyObject.larry.characters[1].name);
