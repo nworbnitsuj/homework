7c3518167a6485e500449547c7d8dc5b55309674
nworbnitsuj <justin.v.brown@gmail.com>
2018-03-28 10:41:20 -0500

    got the js to pull the entry from the form and post it to the console; progress

--- week-two/tuesday/app.js
+++ week-two/tuesday/app.js
@@ -1,9 +1,29 @@
 // console.log("testing");
 
-let getIt = document.getElementById("submitForm").onclick.Submit;
+// Code below made sense on
+// let getIt = document.getElementById("submitForm").onclick;
 //   let text = document.getElementById("newPost").value;
-//   let li = "<li>" + text + "</li>";
-//   document.getElementById("list").appendChild(li);
+//   let ul = "<li>" + text + "</ul>";
+//   document.getElementById("list").appendChild(ul);
 // };
 //
-getIt();
+// getIt();
+
+// const playersForm = document.getElementById("players-form");
+//   playersForm.addEventListener("submit", function(e){
+//     e.preventDefault();
+//     let player1Name = document.getElementById('player-1-name').value;
+//     let player2Name = document.getElementById('player-2-name').value;
+//
+//     console.log(player1Name, player2Name)
+//   });
+
+
+//Pulls user entry from form
+const playersForm = document.getElementById("blogSubmission");
+  playersForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    let newPost = document.getElementById("new-Post").value;
+
+    console.log(newPost)
+  });
