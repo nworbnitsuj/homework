0e97662727902323e5088104849935ffbcb3715e
nworbnitsuj <justin.v.brown@gmail.com>
2018-03-28 09:54:25 -0500

    began styling and added js that does not work

--- week-two/tuesday/app.js
+++ week-two/tuesday/app.js
@@ -1,3 +1,9 @@
-console.log("testing");
+// console.log("testing");
 
-let posts = [];
+let getIt = document.getElementById("submitForm").onclick.Submit;
+//   let text = document.getElementById("newPost").value;
+//   let li = "<li>" + text + "</li>";
+//   document.getElementById("list").appendChild(li);
+// };
+//
+getIt();
